29d7f9ee963bb5284c41b2ba7464554f6ef5601c
Harsh <av15204050@gmail.com>
2026-05-27 15:55:52 +0530

    Changes done

--- src/components/Plan.jsx
+++ src/components/Plan.jsx
@@ -9,7 +9,7 @@ const Plan = () => {
            <h2 className='text-slate-700 text-[42px] font-semibold'>Choose Your plan</h2>
            <p className='text-gray-500 max-w-lg mx-auto'>Start for free and scale up as you grow. Find the pefrect plann for your content creation needs</p>
        </div>
-        <div className='mt-14 max-sm:mx-8'>
+        <div className='mt-14 max-sm:mx-8 hover:translate-y-2 transition-300s cursor-pointer '>
              <PricingTable/>
         </div>
     </div>
